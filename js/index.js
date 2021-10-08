af82e27491481dfeeffd2df948f770959167ed8f
miroshnikoff84 <miroshnikoff84@mail.ru>
2021-10-08 19:43:16 +0300

    add sliders on page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,62 @@
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+$(document).ready(function(){
+	$('.slider-catalog').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:5,
+		speed:1000,
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+	});
+    $('.slider-stock').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:6,
+		speed:1000,
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+	});
+    $('.slider-preview').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		speed:1000,
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+	});
+});
