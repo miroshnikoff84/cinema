76b69dba5a6ad45322ecbf48ce1c24faf1c850c8
miros <miroshnikoff84@mail.ru>
2021-10-26 21:32:37 +0300

    create project width php

---
+++ app/include/header.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="container">
+        <div class="row"><a class="header-promo" href="#"><img src="assets/img/header-promo.jpg" alt="promo"></a></div>
+        <div class="row header-wrap">
+            <div class="col-6 header-left"><a href="<?php echo BASE_URL ?>"><img src="assets/img/logo.svg" alt="" class="icon"></a></div>
+            <div class="col-6 header-right">
+                <a href="#"><span class="city-label">Санкт-Петербург</span><img src="assets/img/city.svg" alt=""
+                                                                                class="icon"></a>
+                <a href="#"><img src="assets/img/user.svg" alt="" class="icon"></a>
+            </div>
+        </div>
+    </div>
+</header>
